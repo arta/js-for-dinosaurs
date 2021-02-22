1851595ab01e4289edfb4c06411a24ad2385bfd0
Martin Fencl <martin@layeredcode.com>
2021-02-22 17:12:26 -0600

    make a main.js build w webpack to replace index.js
    js-for-dinosaurs % ./node_modules/.bin/webpack ./index.js --mode=development
    
    The dev mode is so devs can read the output (i.e. dist/main.js). For prod we'd use --mode=production

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var moment = require('moment');
+
 console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
 // moment is a global variable declared & initialized in moment.js, line 10
