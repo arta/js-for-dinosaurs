52c549c276d2489460f5b95a411fd155c8e11f89
Martin Fencl <martin@layeredcode.com>
2021-02-22 21:04:56 -0600

    add a dev't study note

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var moment = require('moment');
+// The end point is actually ./module_nodes/moment/min/moment.min.js,
+// but node.js knows the location of each npm module
 
 console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
