64251d278f0a6a20c350bdc4403964ddb9665c59
Martin Fencl <martin@layeredcode.com>
2021-02-22 21:45:12 -0600

    configure and use babel
    Configure in webpack.config.js, use by changes in index.js (add couple modern JS features)

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var moment = require('moment');
+import moment from 'moment';
 // The end point is actually ./module_nodes/moment/min/moment.min.js,
 // but node.js knows the location of each npm module
 
 console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
 // moment is a global variable declared & initialized in moment.js, line 10
+
+const name = 'Bob', time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
